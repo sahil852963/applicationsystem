ebae786cda1c40aadf233e19def423619f9fbe0f
sahil852963 <sahilsharma83999@gmail.com>
2025-12-05 18:11:16 +0530

    minor chnage

---
+++ src/component/index.js
@@ -0,0 +1,7 @@
+export ForgotPassword from "./ForgotPassword/ForgotPassword.jsx";
+export ResetPassword from "./ResetPassword/ResetPassword.jsx";
+export Register from "./Register/Register.jsx";
+export Login from "./Login/Login.jsx";
+export ProtectedRoute from "./ProtectedRoute.jsx";
+export PublicRoute from "./PublicRoute.jsx";
+export LeaveForm from "./LeaveForm/LeaveForm.jsx";
